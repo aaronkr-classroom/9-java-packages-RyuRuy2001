8301c938b4e0fe486a8e545225d2de112fd453ce
hjh2252173 <145317483+hjh2252173@users.noreply.github.com>
2024-05-21 14:38:38 +0900

    수정

--- src/com/section04/Ex1012.java
+++ src/com/section04/Ex1012.java
@@ -6,7 +6,7 @@ public class Ex1012 {
 
 	public static void main(String[] args) {
 		StringTokenizer st= new StringTokenizer(
-				"Java", "C", "Python", "JSP", "PHP"
+				"Java"
 				);
 			while (st.hasMoreTokens()) {
 				System.out.println(st.nextToken());
